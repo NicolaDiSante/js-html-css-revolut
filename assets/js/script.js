be68e7dee070958db78a42203e264a6148730757
NicolaDiSante <nickymacha90@gmail.com>
2021-04-21 16:20:29 +0200

    JS First take

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 $(function(){
+
  $('.features').mouseenter(function(){
-   $('.dropdown-menu').toggleClass('active');
+   $('.dropdown-menu').addClass('active');
  })
+ $('.features').mouseleave(function(){
+  $('.dropdown-menu').removeClass('active');
+ })
+
+ $('.plans').mouseenter(function(){
+  $('.dropdown-menu-small').addClass('active');
 })
+$('.plans').mouseleave(function(){
+ $('.dropdown-menu-small').removeClass('active');
+})
+
+
+})
+
+
 
